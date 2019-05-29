d1050c460c4763ec598508d9612883d1fbd6cfed
Lucas de Ávila Martins <lucas.a.martins@ufv.br>
2019-05-28 21:00:58 -0300

    fix web gps error

--- collector/web/main.dart
+++ collector/web/main.dart
@@ -1,59 +1,23 @@
-@JS() // sets the context, in this case being `window`
-library main; // required library declaration
+import 'dart:async';
+import 'dart:js';
 
 import 'package:collector/classes/item_for_sale.dart';
 import 'package:collector/classes/logged_identity.dart';
 import 'package:collector/classes/sale_history.dart';
 import 'package:collector/classes/stored_data.dart';
-import 'package:flutter_web_ui/ui.dart' as ui;
-import 'package:collector/main.dart' as app;
-
-import 'dart:async';
-import 'package:js/js.dart';
 import 'package:collector/classes/location_getter.dart';
 
-// Explica pro dart como acessar esse objeto do JS
-@JS()
-class coordsResponse {
-  external double get latitude;
-  external double get longitude;
-}
-
-// Explica pro dart como acessar esse objeto do JS
-@JS()
-class positionResponse {
-  external coordsResponse get coords;
-}
-
-typedef void show_position_cb(positionResponse response);
-typedef void on_error_cb(dynamic error);
-
-@JS()
-@anonymous // needed along with factory constructor
-class GeolocationOptions {
-  external factory GeolocationOptions({
-    bool enableHighAccuracy,
-    num timeout,
-    num maximumAge,
-  });
-  external bool get enableHighAccuracy;
-  external num get timeout;
-  external num get maximumAge;
-}
-
-// registra essa função do JS no dart
-@JS('navigator.geolocation.getCurrentPosition')
-external void getCurrentPosition(
-    show_position_cb cb, on_error_cb on_error, GeolocationOptions options);
+import 'package:collector/main.dart' as app;
+import 'package:flutter_web_ui/ui.dart' as ui;
 
 // implementa a classe de resposta que extrai os dados
 // da reposta do JS e coloca numa interface mais "bonitinha" em dart
 class LocationWebResponse implements LocationResponse {
   final double latitude;
   final double longitude;
-  LocationWebResponse(positionResponse r)
-      : this.latitude = r.coords.latitude,
-        this.longitude = r.coords.longitude;
+  LocationWebResponse(JsObject r)
+      : this.latitude = r['coords']['latitude'],
+        this.longitude = r['coords']['longitude'];
 }
 
 // converte a função baseada em callback
@@ -61,15 +25,15 @@ class LocationWebResponse implements LocationResponse {
 Future<LocationWebResponse> get_location() {
   Completer<LocationWebResponse> c = Completer();
   try {
-    getCurrentPosition(
-      (r) => c.complete(LocationWebResponse(r)),
-      (r) => c.completeError(r),
-      GeolocationOptions(
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      ),
-    );
+    context['navigator']['geolocation'].callMethod('getCurrentPosition', [
+      (response) => c.complete(LocationWebResponse(response)),
+      (error) => c.completeError(error),
+      JsObject.jsify({
+        'enableHighAccuracy': true,
+        'timeout': 5000,
+        'maximumAge': 0,
+      }),
+    ]);
   } catch (e) {
     print('Erro ao chamar get location');
     print(e);
@@ -83,6 +47,7 @@ main() async {
   await ui.webOnlyInitializePlatform();
   // passa as dependencias que precisam ser implementadas
   // pelo ambiente host
+
   app.main(
     location_getter: get_location,
     stored_data: StoredData(
